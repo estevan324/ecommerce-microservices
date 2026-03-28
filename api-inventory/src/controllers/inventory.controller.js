69404990c3f060462cd0553fec780ffe8fb950f0
Estevan Hernandes Silva de Oliveira <estevanhernandes1@gmail.com>
2026-03-28 17:58:09 -0300

    chore: adicionar tratamento de erros nos controllers

--- api-inventory/src/controllers/inventory.controller.js
+++ api-inventory/src/controllers/inventory.controller.js
@@ -4,26 +4,34 @@ const inventorySchema = require("../schemas/inventory.schema");
 
 const inventoryController = {
   getByProductId: async (req, res) => {
-    const { productId } = req.params;
+    try {
+      const { productId } = req.params;
 
-    const inventory = await inventoryService.getByProductId(productId);
+      const inventory = await inventoryService.getByProductId(productId);
 
-    if (!inventory) return res.sendStatus(204);
+      if (!inventory) return res.sendStatus(204);
 
-    return res.status(200).json(inventory);
+      return res.status(200).json(inventory);
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
+    }
   },
   save: async (req, res) => {
-    const { error } = inventorySchema.validate(req.body);
+    try {
+      const { error } = inventorySchema.validate(req.body);
 
-    if (error) {
-      return res.status(400).json({ error: error.details[0].message });
+      if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+      }
+
+      const request = new SaveInventoryDTO(req.body);
+
+      const inventory = await inventoryService.save(request);
+
+      return res.status(200).json(inventory);
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
     }
-
-    const request = new SaveInventoryDTO(req.body);
-
-    const inventory = await inventoryService.save(request);
-
-    return res.status(200).json(inventory);
   },
 };
 
